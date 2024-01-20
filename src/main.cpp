ec44bc24c188bfff05fbe923d61cd311e08fdb1e
louis <liu.louis91@gmail.com>
2024-01-20 16:15:44 -0700

    removed autotest from main

--- src/main.cpp
+++ src/main.cpp
@@ -68,7 +68,7 @@ lemlib::OdomSensors sensors(
     &imu     // inertial sensor
 );
 
-// create the   chassis
+// create the chassis
 lemlib::Chassis chassis(drivetrain, linearController, angularController,
                         sensors);
 
@@ -82,33 +82,6 @@ void curvature_drive(bool flipDrive = false) {
     leftY *= -1;
   // move the chassis with curvature  chassis
   chassis.arcade(leftY, rightX);
-}
-
-void auto_test() {
-  // example movement: Move to x: 20 and y: 15, and face heading 90. Timeout set
-  // to 4000 ms
-  chassis.moveToPose(20, 15, 90, 4000);
-  // example movement: Move to x: 0 and y: 0 and face heading 270, going
-  // backwards. Timeout set to 4000ms
-  chassis.moveToPose(0, 0, 270, 4000, {.forwards = false});
-  // cancel the movement after it has travelled 10 inches
-  chassis.waitUntil(10);
-  chassis.cancelMotion();
-  // example movement: Turn to face the point x:45, y:-45. Timeout set to 1000
-  // dont turn faster than 60 (out of a maximum of 127)
-  chassis.turnTo(45, -45, 1000, true, 60);
-  // example movement: Follow the path in path.txt. Lookahead at 15, Timeout set
-  // to 4000 following the path with the back of the robot (forwards = false)
-  // see line 116 to see how to define a path
-  //  chassis.follow(example_txt, 15, 4000, false);
-  // wait until the   chassis has travelled 10 inches. Otherwise the code
-  // directly after the movement will run immediately Unless its another
-  // movement, in which case it will wait
-  chassis.waitUntil(10);
-  pros::lcd::print(4, "Travelled 10 inches during pure pursuit!");
-  // wait until the movement is done
-  chassis.waitUntilDone();
-  pros::lcd::print(4, "pure pursuit finished!");
 }
 
 void initialize() {
