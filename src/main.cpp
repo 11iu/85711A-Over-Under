a40f5ada9efbacc30c382d87f5434e244e01b5ba
louis <liu.louis91@gmail.com>
2024-03-01 15:52:01 -0700

    logging test for distance

--- src/main.cpp
+++ src/main.cpp
@@ -188,6 +188,10 @@ void opcontrol() {
 
     while (true)
     {
+        // log distance sensor
+        int reading = distBack.get_value();
+        pros::lcd::print(0, "%i", reading);
+
         // testing autos
 
         if (master.get_digital(pros::E_CONTROLLER_DIGITAL_UP))
