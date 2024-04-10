5606934a01e12406e3817ff2ea0d252aa8aca6aa
louis <liu.louis91@gmail.com>
2024-04-10 14:34:43 -0700

    maybe fix auto far

--- src/autons.cpp
+++ src/autons.cpp
@@ -216,26 +216,26 @@ void Autons::autoFarInsane()
   chassis.moveToPose(blueGoalCenter.x - tile * 0.4, farInsaneStart.y, 180, 1000, {.minSpeed = 80}, false);                   // turn to face goal
   intake = 127;
   chassis.moveToPose(blueGoalCenter.x - tile * 0.4, blueGoalCenter.y + 2, 180, 1500, {.minSpeed = 100}, false);                      // Shoves preload in
-  chassis.moveToPose(blueGoalCenter.x - tile * 0.4, blueGoalCenter.y + tile, 180, 1500, {.forwards = false, .minSpeed = 80}, false); // back out
-  chassis.moveToPose(blueGoalCenter.x - tile * 0.4, blueGoalCenter.y + tile, 0, 1500, {.minSpeed = 80}, false);                      // turn towards triball
+  chassis.moveToPose(blueGoalCenter.x - tile * 0.4, blueGoalCenter.y + tile * 0.7, 180, 1500, {.forwards = false, .minSpeed = 80}, false); // back out
+  chassis.moveToPose(blueGoalCenter.x - tile * 0.4, blueGoalCenter.y + tile * 0.7, -5, 1500, {.forwards = false, .minSpeed = 80}, false);                      // turn towards triball
   intake = 0;
 
   // pick up triball to the left side
   intake = -127;
-  chassis.moveToPose(redCenterLeftTriball.x + 4, redCenterLeftTriball.y - 10, 0, 800, {.maxSpeed = 100}, false); // pick up
+  chassis.moveToPose(redCenterLeftTriball.x + 4, redCenterLeftTriball.y - 10, -5, 800, {.maxSpeed = 80}, false); // pick up
   intake = 0;
 
   // moving backwards to get that lower triball with wings
-  chassis.moveToPose(redCenterLeftTriball.x + 4, redCenterLeftTriball.y - 10, -45, 800, {.forwards = false}, false);         // turn
+  chassis.moveToPose(redCenterLeftTriball.x + 4, redCenterLeftTriball.y - 10, -45, 800, {}, false);         // turn
   chassis.moveToPose(redCenterLowerTriball.x - tile * 0.5, tile * 2, -45, 1000, {.forwards = false, .minSpeed = 60}, false); // back up into the lower triball
   setWings(HIGH);
-  chassis.moveToPose(redCenterLowerTriball.x - tile * 0.5, tile * 2, 45, 1000, {}, false); // turn a lot to swing the triball around
+  chassis.moveToPose(redCenterLowerTriball.x - tile * 0.5, tile * 2, 10, 1000, {}, false); // turn to swing the triball around
 
   // score the left and lower triballs
-  chassis.moveToPose(blueGoalCenter.x, tile * 2.5, 0, 1200, {.minSpeed = 80}, false);   // move forward
-  chassis.moveToPose(blueGoalCenter.x, tile * 2.5, 180, 1000, {.minSpeed = 80}, false); // turn back around
+  chassis.moveToPose(blueGoalCenter.x, tile * 2.2, 0, 1200, {.minSpeed = 80}, false);   // move forward
+  chassis.moveToPose(blueGoalCenter.x, tile * 2.2, 180, 1000, {.minSpeed = 80}, false); // turn back around
   intake = 127;
-  chassis.moveToPose(blueGoalCenter.x, blueGoalCenter.y, 180, 2000, {.minSpeed = 100}, false); // score
+  chassis.moveToPose(blueGoalCenter.x, blueGoalCenter.y, 180, 2000, {.minSpeed = 100}, false); // score both
   intake = 0;
   chassis.moveToPose(blueGoalCenter.x, blueGoalCenter.y + tile, 180, 1200, {.forwards = false, .minSpeed = 80}, false); // back out
   chassis.moveToPose(blueGoalCenter.x, blueGoalCenter.y + tile, 0, 800, {}, false);                                     // turn towards center
@@ -254,7 +254,7 @@ void Autons::autoFarInsane()
   // reset for teleop
   chassis.moveToPose(blueGoalCenter.x, blueGoalCenter.y + tile, 180, 2000, {.forwards = false, .minSpeed = 100}, false); // reset
   intake = 0;
-  chassis.moveToPose(blueGoalCenter.x, blueGoalCenter.y + tile * 0.5, 0, 2000, {.minSpeed = 100}, false); // reset
+  chassis.moveToPose(blueGoalCenter.x, blueGoalCenter.y + tile, 0, 2000, {.minSpeed = 100}, false); // reset
 }
 
 // starts in far side facing the goal, completes awp(score, descore, touch bar)
